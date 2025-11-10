a13490aaf8c5283c9bc9a314ad784386e46d8c58
Vainilla <larissitagarcia0703@gmail.com>
2025-11-09 20:13:14 -0600

    Carpetas esqueleto de clases e interfaces
    Se realizaron en development porque se requiere que todos las tengan

--- P_BlackJack_Uno/obj/Debug/net9.0/P_BlackJack_Uno.AssemblyInfo.cs
+++ P_BlackJack_Uno/obj/Debug/net9.0/P_BlackJack_Uno.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P_BlackJack_Uno")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07fd718953417f8f23b0e9da5c565118eb17daa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("P_BlackJack_Uno")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P_BlackJack_Uno")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
